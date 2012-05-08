85964f52d1d9459e46d40a67ce48202a03d28d26
John C. Bland II <iam@johncblandii.com>
2012-05-08 13:58:39 -0500

    Added book support and refactored page to use conclusion? only

--- adventure.rb
+++ adventure.rb
@@ -1,5 +1,21 @@
 require 'rubygems'
 require 'bundler/setup'
+require_relative "db/setup"
+require_relative "models/page"
+require_relative "models/book"
 
+page = Page.create(starting_point: true, content: "You woke up on a road. It's foggy and dampy. In your bag is 30 gold pieces and a bacon sandwich. Which do you choose?")
+Page.create(conclusion: true, parent_id: page.id, content: "WINNER")
+Page.create(conclusion: true, parent_id: page.id, content: "LOSER")
 
-puts "sanity check"
+book = Book.new(page)
+
+until book.complete_game? do
+	puts book.current_page.content
+	puts "your options: "
+	puts " - [#{book.current_page.options.first}"
+	puts " - [#{book.current_page.options.last}"
+	puts "What do ou want to do? Enter A or B"
+
+	book.input(gets)
+end
--- models/page.rb
+++ models/page.rb
@@ -7,8 +7,4 @@ class Page < ActiveRecord::Base
 	def options
 		Page.where(:parent_id=>id).limit(2)
 	end
-
-	def end_of_road?
-		conclusion?
-	end
 end
